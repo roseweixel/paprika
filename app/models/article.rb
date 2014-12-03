3bd03a587805fff3b0548d991ed17ee2c29d24e8
Rose Weixel <rose.weixel@gmail.com>
2014-12-02 23:24:12 -0500

    start working on nytimes api calls

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,4 +7,10 @@ class Article < ActiveRecord::Base
   # Thinh will pass Rose an array of trending stories from Twitter.
   # Rose will iterate over that array and pull articles from the NYTimes (keyword?).
 
+  def get_datetime
+    # this is how to get the date as a datetime object given a single article in the response hash from a NYTimes API call
+    DateTime.iso8601(response["response"]["docs"].first["pub_date"])
+  end
+
+
 end
