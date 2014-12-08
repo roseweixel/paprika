4123fca5b6ee71a31fd613663e2b41410197f8c9
Rose Weixel <rose.weixel@gmail.com>
2014-12-07 19:32:06 -0500

    clean some comments

--- app/models/story.rb
+++ app/models/story.rb
@@ -6,7 +6,7 @@ class Story < ActiveRecord::Base
   has_many :articles
   has_many :scores
 
-  # returns an array of 500 most popular articles (from most to least popular)
+  # returns an array of 800 most popular articles (from most to least popular)
   def get_most_populars
     article_array = []
     offset = 0
@@ -20,48 +20,12 @@ class Story < ActiveRecord::Base
     article_array
   end
 
+  # creates Article objects
   def create_articles_for_story
     all_populars = get_most_populars
     all_populars.each_with_index do |article, rank|
-      #binding.pry
       if article["title"] =~ /#{self.name}/i || article["abstract"] =~ /#{self.name}/i
-        #binding.pry
         self.articles.find_or_create_by(:name => article["title"], :abstract => article["abstract"], :url => article["url"], :rank => rank+1, :published_date =>  DateTime.iso8601(article["published_date"]))
       end
     end
   end
-  #des_facet
-  # def articles
-  #   type = "json"
-
-  #   url = search_url_basic
-
-  #   response_hash = JSON.load(open(url))
-
-  #   articles = response_hash["response"]["docs"]
-  # end
-
-  # def search_url_basic
-  #   "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=#{timesified_name}&api-key=#{ENV["ny_times_article_search_key"]}"
-  # end
-
-  def make_articles_for(get_most_populars)
-    # iterate over the most populars looking for self.name
-      # GO THROUGH THESE STEPS AND WHEN FOUND, MAKE ARTICLE
-      # look in the title
-      # look in the abstract
-      # whatever else there is to look in
-    # parse the 'rank' (the index in the array), the date, and the article object itself
-  end
-
-end
-
-# #{ENV[ny_times_most_popular_key]}
-
-# /svc/mostpopular/v2/{mostemailed | mostshared | mostviewed}/
-# sections-list[.response-format]?api-key={your-API-key}
-
-# http://api.nytimes.com/svc/mostpopular/v2/mostviewed/arts/30.xml?offset=40
-
-# http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/30.xml?offset=0&api-key=c2f0ef1ae7ae6dae568c98e040255a45:11:70265638
-
