4d9df7e08852a9eee03d2e268ebeec5d08e2fbdb
thinhthinh <thinhqvu@gmail.com>
2014-12-08 15:06:45 -0500

    Add footer

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -13,8 +13,8 @@ $(document).ready(function(){
     $this.append(bulletHTML)
     
     //Appending Rank Y-Axis Title
-    $('.chart-row .rank').remove();
-    $('.chart-row').prepend('<div class="rank col-md-1">Rank</div>')
+    // $('.chart-row .rank').remove();
+    // $('.chart-row').prepend('<div class="rank col-md-1">Rank</div>')
   
     //Creating the chart
     $.ajax({url: $this.attr('href'), dataType: 'JSON'}).done(function(result){
@@ -42,8 +42,8 @@ $(document).ready(function(){
       var graph = new Rickshaw.Graph( {
         element: document.querySelector("#chart"),
         renderer: 'line',
-        width: 680,
-        height: 350,
+        width: 900,
+        height: 400,
         min: -50,
         max: 850,
         series: [ {
